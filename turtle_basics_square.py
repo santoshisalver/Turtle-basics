f1407a91c1f673b65f227ff14e83e46381806198
santoshisalver <santoshisalver36@gmail.com>
2024-11-18 10:21:18 +0530

    commit both the files

---
+++ turtle_basics_square.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 09:47:59 2024
+
+@author: santo
+"""
+
+import turtle
+tur=turtle.Turtle()
+for i in range(20):
+    tur.forward(50)
+    tur.right(90)
+turtle.done()
